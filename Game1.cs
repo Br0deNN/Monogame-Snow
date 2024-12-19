badee331de2237658e33b9a42cd80eb646d3a1c0
D7a8 <theo.broden@gmail.com>
2024-12-19 13:27:28 +0100

    klar

--- Game1.cs
+++ Game1.cs
@@ -30,7 +30,7 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
-        Texture2D pixel = Content.Load<Texture2D>("pixel");
+        Texture2D pixel = Content.Load<Texture2D>("snowfalke");
         particleSystem = new ParticleSystem(pixel);
     }
 
@@ -48,7 +48,7 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.DarkRed);
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
